732af37a4386215e86658c87f2347c5b74d8c0b1
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-08-24 22:34:46 +0000

    automatically backed up by learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,18 +1,3 @@
 class Preference < ActiveRecord::Base
   
-  def song_sort_order
-    Song.order(name: :asc)
-  end
-  
-  def artist_sort_order
-    Artist.order(name: :asc)
-  end
-  
-  def allow_create_artists
-  end
-  
-  def allow_create_songs
-  end
-  
-  
 end
